5d82692c29ee1c1ebf264cd27c5fc67b4a744c17
Robert WEILER <rbrt-weiler@mailbox.org>
2020-02-09 21:37:41 +0100

    Added functions for all int types.

--- envordef.go
+++ envordef.go
@@ -53,6 +53,50 @@ func IntVal(envName string, defaultVal int) int {
 	return retVal
 }
 
+// Int8Val returns the value of environment variable envName as an int or the supplied default value defaultVal if the environment variable does not exist.
+func Int8Val(envName string, defaultVal int8) int8 {
+	retVal := defaultVal
+	envVal, envExists := os.LookupEnv(envName)
+	if envExists {
+		intVal, _ := strconv.Atoi(envVal)
+		retVal = int8(intVal)
+	}
+	return retVal
+}
+
+// Int16Val returns the value of environment variable envName as an int or the supplied default value defaultVal if the environment variable does not exist.
+func Int16Val(envName string, defaultVal int16) int16 {
+	retVal := defaultVal
+	envVal, envExists := os.LookupEnv(envName)
+	if envExists {
+		intVal, _ := strconv.Atoi(envVal)
+		retVal = int16(intVal)
+	}
+	return retVal
+}
+
+// Int32Val returns the value of environment variable envName as an int or the supplied default value defaultVal if the environment variable does not exist.
+func Int32Val(envName string, defaultVal int32) int32 {
+	retVal := defaultVal
+	envVal, envExists := os.LookupEnv(envName)
+	if envExists {
+		intVal, _ := strconv.Atoi(envVal)
+		retVal = int32(intVal)
+	}
+	return retVal
+}
+
+// Int64Val returns the value of environment variable envName as an int or the supplied default value defaultVal if the environment variable does not exist.
+func Int64Val(envName string, defaultVal int64) int64 {
+	retVal := defaultVal
+	envVal, envExists := os.LookupEnv(envName)
+	if envExists {
+		intVal, _ := strconv.Atoi(envVal)
+		retVal = int64(intVal)
+	}
+	return retVal
+}
+
 // UintVal returns the value of environment variable envName as an uint or the supplied default value defaultVal if the environment variable does not exist.
 func UintVal(envName string, defaultVal uint) uint {
 	retVal := defaultVal
