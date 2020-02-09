46197880c65f1094a2542664573f837b97ae3d2f
Robert WEILER <rbrt-weiler@mailbox.org>
2020-02-09 21:49:21 +0100

    Switched to ParseInt() for int types.

--- envordef.go
+++ envordef.go
@@ -75,45 +75,45 @@ func IntVal(envName string, defaultVal int) int {
 	return retVal
 }
 
-// Int8Val returns the value of environment variable envName as an int or the supplied default value defaultVal if the environment variable does not exist.
+// Int8Val returns the value of environment variable envName as an int8 or the supplied default value defaultVal if the environment variable does not exist.
 func Int8Val(envName string, defaultVal int8) int8 {
 	retVal := defaultVal
 	envVal, envExists := os.LookupEnv(envName)
 	if envExists {
-		intVal, _ := strconv.Atoi(envVal)
+		intVal, _ := strconv.ParseInt(envVal, 10, 8)
 		retVal = int8(intVal)
 	}
 	return retVal
 }
 
-// Int16Val returns the value of environment variable envName as an int or the supplied default value defaultVal if the environment variable does not exist.
+// Int16Val returns the value of environment variable envName as an int16 or the supplied default value defaultVal if the environment variable does not exist.
 func Int16Val(envName string, defaultVal int16) int16 {
 	retVal := defaultVal
 	envVal, envExists := os.LookupEnv(envName)
 	if envExists {
-		intVal, _ := strconv.Atoi(envVal)
+		intVal, _ := strconv.ParseInt(envVal, 10, 16)
 		retVal = int16(intVal)
 	}
 	return retVal
 }
 
-// Int32Val returns the value of environment variable envName as an int or the supplied default value defaultVal if the environment variable does not exist.
+// Int32Val returns the value of environment variable envName as an int32 or the supplied default value defaultVal if the environment variable does not exist.
 func Int32Val(envName string, defaultVal int32) int32 {
 	retVal := defaultVal
 	envVal, envExists := os.LookupEnv(envName)
 	if envExists {
-		intVal, _ := strconv.Atoi(envVal)
+		intVal, _ := strconv.ParseInt(envVal, 10, 32)
 		retVal = int32(intVal)
 	}
 	return retVal
 }
 
-// Int64Val returns the value of environment variable envName as an int or the supplied default value defaultVal if the environment variable does not exist.
+// Int64Val returns the value of environment variable envName as an int64 or the supplied default value defaultVal if the environment variable does not exist.
 func Int64Val(envName string, defaultVal int64) int64 {
 	retVal := defaultVal
 	envVal, envExists := os.LookupEnv(envName)
 	if envExists {
-		intVal, _ := strconv.Atoi(envVal)
+		intVal, _ := strconv.ParseInt(envVal, 10, 64)
 		retVal = int64(intVal)
 	}
 	return retVal
